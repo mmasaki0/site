7af2bf6df65355a1ce145340e5f64d596b14e380
mmasaki0 <111476395+mmasaki0@users.noreply.github.com>
2026-06-08 00:04:34 -0500

    feat: placeholder assets and highlight on nav

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
+function highlight(e) {
+    e.style.transition = "text-shadow 1ms ease-in-out"
+    e.style.textShadow = "white 0 0 4px";
+    
+    setTimeout(() => {
+        e.style.transition = "text-shadow 1000ms ease-in-out"
+        e.style.textShadow = "rgba(0,0,0,0) 0 0 0";
+    }, 500);
+}
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
+        highlight(document.querySelector(this.getAttribute('href')).nextElementSibling);
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
+
